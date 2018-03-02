8ec64e469f1444a6ab945e3283179104853c3a6b
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-03 03:26:50 +0500

    Update finish.js

--- BeeApple/images/bash_test/js/finish.js
+++ BeeApple/images/bash_test/js/finish.js
@@ -2,12 +2,29 @@ var Finish ={
 	preload : function(){},
 	create: function(){
 		instruction = game.add.text(game.world.width/2,game.world.height/2,
-			"За 15 секунд вы верно ответили\n на "+score+" вопросов\n Нажмите, чтобы поделиться",
+			"За 15 секунд вы верно ответили\n на "+score+this.whatSl("вопрос")+"\n Нажмите, чтобы поделиться",
 			{font:"bold 23px Arial",fill:'#000000', align:'center'});
 		instruction.anchor.set(0.5,0.5);	
 		game.input.onDown.add(this.wallPost,this);	},
 	update: function(){},
-	wallPost: function(){VK.api("wall.post", {"message": "Hello!"}, function (data) {
-						alert("Post ID:" + data.response.post_id);
-});}
+	wallPost: function(){
+		VK.api("wall.post", {"message": String("В игре Udareniya я веро расставл ударения на "+score+this.whatSl("слово")), "attachments": "photo-160039023_456239018"}
+	},
+	whatSl: function(sl){
+		var ret=sl;
+		if(sl=='вопрос'){
+			if(score==0 || score>=5){
+				ret="вопросов";
+			}else if(score>1 && score<5){
+				ret="вопроса";
+			}
+		}else if(sl=='слово'){
+			if(score==0 || score>=5){
+				ret="слов";
+			}else if(score>1 && score<5){
+				ret="слова";
+			}
+		}
+		return ret;
+	}
 }
