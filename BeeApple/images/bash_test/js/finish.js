17f668bcd63a4f76ae9ecaaa28326f389b1ccb0b
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-03 03:31:39 +0500

    Update finish.js

--- BeeApple/images/bash_test/js/finish.js
+++ BeeApple/images/bash_test/js/finish.js
@@ -2,13 +2,13 @@ var Finish ={
 	preload : function(){},
 	create: function(){
 		instruction = game.add.text(game.world.width/2,game.world.height/2,
-			"За 15 секунд вы верно ответили\n на "+score+this.whatSl("вопрос")+"\n Нажмите, чтобы поделиться",
+			"За 15 секунд вы верно ответили\n на "+score+this.whatSl("вопрос")+"\n Кликните, чтобы поделиться",
 			{font:"bold 23px Arial",fill:'#000000', align:'center'});
 		instruction.anchor.set(0.5,0.5);	
 		game.input.onDown.add(this.wallPost,this);	},
 	update: function(){},
 	wallPost: function(){
-		VK.api("wall.post", {"message": String("В игре Udareniya я веро расставл ударения на "+score+this.whatSl("слово")), "attachments": "photo-160039023_456239018"}
+		VK.api("wall.post", {"message": String("В игре Udareniya я верно расставил ударения на "+score+this.whatSl("слово")+"  Ссылка: vk.com/app6393619"), "attachments": "photo-160039023_456239018"});
 	},
 	whatSl: function(sl){
 		var ret=sl;
@@ -25,6 +25,6 @@ var Finish ={
 				ret="слова";
 			}
 		}
-		return ret;
+		return String(' '+ret);
 	}
 }
