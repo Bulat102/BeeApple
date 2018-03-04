5ab696180e8e7d0126bf3cc991237f59eaf743b7
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-05 00:54:08 +0500

    Update finish.js

--- BeeApple/images/Bash_test/js/finish.js
+++ BeeApple/images/Bash_test/js/finish.js
@@ -54,7 +54,7 @@ var Finish ={
 			names.splice(n,0,name);
 			scores.splice(n,0,score);
 			scoresText.text="Рекорды:\n1. "+names[0]+" "+scores[0]+"\n2. "+names[1]+" "+scores[1]+"\n3. "+names[2]+" "+scores[2];
-			code =String('return[API.storage.set({"key":"top1","value": \''+scores[0]+'\',"global":1}),API.storage.set({"key":"top2","value": \''+scores[1]+'\',"global":1}),API.storage.set({"key":"top3","value":\''+scores[2]+'\',"global":1}),API.storage.set({"key":"name1","value":\''+names[0]+'\',"global":1}),API.storage.set({"key":"name2","value":\''+names[1]+'\',"global":1}),API.storage.set({"key":"name3","value": \''+names[2]+'\',"global":1})];');
+			code =String('return[API.storage.set({"key":"top1","value": \''+scores[0]+'\',"global":1})];');
 			VK.api("execute",{"code":code},function(data){instruction.text=JSON.stringify(data,"",4);});
 		}else{
 			scoresText.text="Рекорды:\n1. "+names[0]+" "+scores[0]+"\n2. "+names[1]+" "+scores[1]+"\n3. "+names[2]+" "+scores[2];
