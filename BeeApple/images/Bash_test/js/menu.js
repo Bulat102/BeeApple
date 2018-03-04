b92f1bbbed384f0f6db9cf11bca6385421bc1c79
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-04 10:50:51 +0500

    Update menu.js

--- BeeApple/images/Bash_test/js/menu.js
+++ BeeApple/images/Bash_test/js/menu.js
@@ -8,13 +8,13 @@ var Menu={
 			"Выберите верный ответ,\n кликнув на него.\n Кликните, чтобы начать",
 			{font:"bold 23px Arial",fill:'#000000', align:'center'});
 		instruction.anchor.set(0.5,0);	
-		game.input.onDown.add(this.startGame,this);	
-	},
-	startGame: function(){
 		var score1;
 		VK.api("storage.get",{ "key": "top1","global": 1},function(data){score1=data.response;});
 		VK.api("storage.get",{ "key": "name1","global": 1},function(data){instruction.text=String(
 			instruction.text+"\nЛидер: "+String(data.last_name+" "+data.first_name)+" "+score1);});
+		game.input.onDown.add(this.startGame,this);	
+	},
+	startGame: function(){
 		instruction.destroy();
 		game.state.start("Game");
 	},
