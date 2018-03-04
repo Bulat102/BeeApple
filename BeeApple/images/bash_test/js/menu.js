71c9fbae9887aa7c3ac47c3e37fdbe775c685b5f
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-04 09:37:32 +0500

    Update menu.js

--- BeeApple/images/bash_test/js/menu.js
+++ BeeApple/images/bash_test/js/menu.js
@@ -11,7 +11,7 @@ var Menu={
 		game.input.onDown.add(this.startGame,this);	
 	},
 	startGame: function(){
-		VK.api("storage.get",{ "key": "scores1","global": 1},function(data){instruction.text=instruction.text+"\nЛидер: "+data.response});
+		VK.api("storage.get",{ "key": "scores1","global": 1},function(data){instruction.text=String(instruction.text+"\nЛидер: "+data.response)});
 		//instruction.destroy();
 		//game.state.start("Game");
 	},
