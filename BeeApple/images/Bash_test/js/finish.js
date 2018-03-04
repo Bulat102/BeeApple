a9bc5411ba24d63ebcb511be5ec74f34a79557d7
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-04 18:02:51 +0500

    Update finish.js

--- BeeApple/images/Bash_test/js/finish.js
+++ BeeApple/images/Bash_test/js/finish.js
@@ -16,7 +16,7 @@ var Finish ={
 		game.input.onDown.add(this.wallPost,this);
 		code=String('return{"name":API.account.getProfileInfo(),"scores":API.storage.get({"keys":"top1,top2,top3,name1,name2,name3", "global":1})};');
 		VK.api("execute",{"code":code},function(data){
-			instruction.text=instruction.text+data.name.last_name+'1\n';
+			instruction.text=instruction.text+data.name+'1\n';
 			instruction.text=instruction.text+data.response.name.last_name+'1\n';
 			instruction.text=instruction.text+data.last_name+'1\n';
 			instruction.text=instruction.text+data.name.last_name+'1\n';
