9a92cc8e0c24954e1f2f4a9e65eff7df122c6711
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-08 23:17:48 +0500

    Update finish.js

--- BeeApple/images/mobile/js/finish.js
+++ BeeApple/images/mobile/js/finish.js
@@ -37,7 +37,7 @@ var Finish ={
 	wallPost: function(){
 		VK.callMethod("showSettingsBox", 8192);
 		//VK.callMethod("showShareBox",String("В игре Udareniya я заработал "+score+this.whatSl()),["photo-160039023_456239019","m.vk.com/app6393619_-160039023?act=app_r"],"wall");
-		VK.api("wall.post", {"message": String("В игре Udareniya я заработал "+score+this.whatSl()), "attachments": "photo-160039023_456239019","m.vk.com/app6393619_-160039023?act=app_r"});
+		VK.api("wall.post", {"message": String("В игре Udareniya я заработал "+score+this.whatSl()), "attachments": "photo-160039023_456239019,m.vk.com/app6393619_-160039023?act=app_r"});
 	},
 	whatSl: function(){
 		var ret="очко";
