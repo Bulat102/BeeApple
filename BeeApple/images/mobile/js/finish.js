8d9cd869b02b06881b8f952109de16863aa671e9
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-03-09 12:29:16 +0500

    Update finish.js

--- BeeApple/images/mobile/js/finish.js
+++ BeeApple/images/mobile/js/finish.js
@@ -60,7 +60,7 @@ var Finish ={
 			scoresText.text="Рекорды:\n1. "+names[0]+" "+scores[0]+"\n2. "+names[1]+" "+scores[1]+"\n3. "+names[2]+" "+scores[2];
 			code =String('return[API.storage.set({"key":"top1","value": \"'+scores[0]+'\","global":1}),API.storage.set({"key":"top2","value": \"'+scores[1]+'\","global":1}),API.storage.set({"key":"top3","value":\"'+scores[2]+'\","global":1}),API.storage.set({"key":"name1","value":\"'+names[0]+'\","global":1}),API.storage.set({"key":"name2","value":\"'+names[1]+'\","global":1}),API.storage.set({"key":"name3","value": \"'+names[2]+'\","global":1})];');
 			scoresText.text=scoresText.text+"\n\n Результат "+score+" попал в топ)";
-			VK.api("execute",{"code":code});
+		//	VK.api("execute",{"code":code});
 		}else{
 			scoresText.text="Рекорды:\n1. "+names[0]+" "+scores[0]+"\n2. "+names[1]+" "+scores[1]+"\n3. "+names[2]+" "+scores[2];
 			scoresText.text=scoresText.text+"\n\n Результат "+score+" не попал в топ(";
