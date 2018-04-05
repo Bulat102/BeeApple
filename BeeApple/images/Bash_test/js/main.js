3d12739a147276bef8d0fdf90a4897a8c42dba50
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-04-05 21:25:48 +0500

    Update main.js

--- BeeApple/images/Bash_test/js/main.js
+++ BeeApple/images/Bash_test/js/main.js
@@ -1,7 +1,7 @@
 var game;
 VK.init(function() {}, function() {}, '5.74');
 window.onload=function(){
-	VK.callMethod("scrollWindow", 200);
+	VK.callMethod("scrollWindow", 150);
 	game = new Phaser.Game(800,600,Phaser.AUTO,'Bash_test');
 	game.state.add("Menu",Menu);
 	game.state.add("Game",Game);
