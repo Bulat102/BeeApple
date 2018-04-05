a4972f37a6649247d6d8bdc118cb6f4712cf915a
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-04-05 21:22:30 +0500

    Update main.js

--- BeeApple/images/mobile/js/main.js
+++ BeeApple/images/mobile/js/main.js
@@ -3,6 +3,7 @@ var koefx;//коэффициент экрана
 var koefy;
 VK.init(function() {console.log("success")}, function() {console.log("error")}, '5.74');
 window.onload=function(){
+    VK.callMethod("scrollWindow", 200);
     koefx=window.innerWidth/900;
     game = new Phaser.Game(window.innerWidth-50*koefx, window.innerHeight-100*koefx, Phaser.AUTO, "ph_game");
 	game.state.add("Menu",Menu);
