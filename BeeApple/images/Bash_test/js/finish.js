efd512a2b5aaeffa4e1565ce16444de10a165c9c
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2018-04-13 21:25:41 +0500

    Update finish.js

--- BeeApple/images/Bash_test/js/finish.js
+++ BeeApple/images/Bash_test/js/finish.js
@@ -50,13 +50,13 @@ var Finish ={
 		var addBool=true;
 		for(var i=scores.length-2;i>=0;i--){
 			if(String(name)==String(names[i])){
+					console.log(names);
 				if(Number(scores[i])>=Number(score)){
 					addBool=false;
 					break;
 				}else{
 					names.splice(i,1);
 					scores.splice(i,1);
-					console.log(names);
 					break;
 				}
 			}
